b42cee4856f0f140bb892c46b3cd575861021e93
Newland-A <ajn252@gmail.com>
2020-08-29 11:24:10 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post
+  #sets getters and a setters for title and author
+  attr_accessor :title, :author
+
+end
+#post.author = author
+#author = Author.new
---
+++ lib/song.rb
@@ -0,0 +1,5 @@
+class Song
+  #sets getters and setters for the title of the song and the artist that sings the song
+  attr_accessor :title, :artist
+  
+end
